d3071daf65c58540ea1330d3d976055571b79f95
Daniela Fernández <liquiddookie.17@gmail.com>
2024-10-02 20:08:43 -0300

    Updated.

--- TP2/EntregaFinal/js/home_script.js
+++ TP2/EntregaFinal/js/home_script.js
@@ -2,22 +2,39 @@
 const carousels = document.querySelectorAll(".carousel-wrap");
 
 carousels.forEach(carouselWrap => {
-    let scrollContainer = document.querySelector(".carousel-container");
+    let scrollContainer = carouselWrap.querySelector(".carousel-container");
 
-    let prevBtn = document.querySelector(".prev-button");
-    let nextBtn = document.querySelector(".next-button");
-    let cards = document.querySelectorAll(".card");
+    let prevBtn = carouselWrap.querySelector(".prev-button");
+    let nextBtn = carouselWrap.querySelector(".next-button");
+    let cards = carouselWrap.querySelectorAll(".card");
 
-    let card = document.querySelector(".card");
+    let card = carouselWrap.querySelector(".card");
     const movement = card.offsetWidth;
+
+    let atEnd = false; // Indicador de si estamos al final
 
     prevBtn.addEventListener("click", () => {
         scrollContainer.style.scrollBehavior = "smooth";
         scrollContainer.scrollLeft -= movement;
+        atEnd = false; // Reiniciamos indicador al desplazarse hacia atrás
     });
 
     nextBtn.addEventListener("click", () => {
-        scrollContainer.style.scrollBehavior = "smooth";
-        scrollContainer.scrollLeft += movement;
+        if (atEnd) {
+            // Si estamos al final, reseteamos al inicio
+            scrollContainer.scrollLeft = 0;
+            atEnd = false; // Reiniciamos el indicador
+        } else {
+            scrollContainer.style.scrollBehavior = "smooth";
+            scrollContainer.scrollLeft += movement;
+        }
+    });
+
+    scrollContainer.addEventListener('scroll', () => {
+        // Verificar si se ha alcanzado el final del scroll
+        if (scrollContainer.scrollLeft + scrollContainer.clientWidth >= scrollContainer.scrollWidth) {
+            atEnd = true; // Indicamos que hemos llegado al final
+        }
     });
 });
+
