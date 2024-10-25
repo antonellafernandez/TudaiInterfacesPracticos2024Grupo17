f10b71d28925b5d77a3a49cffe9128ed26de1a4b
Daniela Fernández <liquiddookie.17@gmail.com>
2024-10-24 22:15:04 -0300

    Updated.

---
+++ TP3/EntregaFinal/js/home_script.js
@@ -0,0 +1,141 @@
+// Función para el loading
+document.addEventListener('DOMContentLoaded', function () {
+    const loadingContainer = document.getElementById('loading');
+    const content = document.getElementById('content');
+    const percentageText = document.getElementById('percentage');
+
+    let progress = 0;
+    const loadingInterval = setInterval(() => {
+        progress += 1; // Aumentar el progreso en 1% cada 100ms
+        percentageText.textContent = `${progress}%`;
+
+        if (progress >= 100) {
+            clearInterval(loadingInterval); // Detener el intervalo
+            loadingContainer.style.display = 'none'; // Ocultar el loading
+            content.style.display = 'block'; // Mostrar el contenido
+        }
+    }, 50); 
+}); 
+
+// Animación carouseles
+// Función de easing (aceleración rápida y desaceleración lenta)
+function easeInOutCubic(progress) {
+    if (progress < 0.5) {
+        return 4 * progress * progress * progress;
+    }
+    return 1 - Math.pow(-2 * progress + 2, 3) / 2;
+}
+
+// Función personalizada de desplazamiento suave con animación
+function smoothScroll(scrollContainer, distance, duration) {
+    const start = scrollContainer.scrollLeft;
+    let startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const progress = Math.min(timeElapsed / duration, 1);
+
+        const easing = easeInOutCubic(progress);
+        scrollContainer.scrollLeft = start + distance * easing;
+
+        if (timeElapsed < duration) {
+            requestAnimationFrame(animation);
+        }
+    }
+
+    requestAnimationFrame(animation);
+}
+
+// Función para agregar evento scroll a cada carousel
+function addScrollEvent(carousel) {
+    let lastScrollPosition = 0;
+    const cards = carousel.querySelectorAll('.card, .featured-card');
+
+    carousel.addEventListener('scroll', () => {
+        const scrollX = carousel.scrollLeft;
+        const scrollDirection = scrollX > lastScrollPosition ? 'right' : 'left';
+        lastScrollPosition = scrollX;
+
+        if (scrollDirection === 'right') {
+            cards.forEach((card) => {
+                card.classList.add('animate-scale-right');
+                card.classList.remove('animate-scale-left');
+            });
+        } else if (scrollDirection === 'left') {
+            cards.forEach((card) => {
+                card.classList.add('animate-scale-left');
+                card.classList.remove('animate-scale-right');
+            });
+        }
+    });
+}
+
+// Seleccionar todos los carruseles
+const carousels = document.querySelectorAll(".carousel-wrap");
+
+carousels.forEach(carouselWrap => {
+    const scrollContainer = carouselWrap.querySelector(".carousel-container");
+    const carousel = carouselWrap.querySelector(".carousel");
+    const prevBtn = carouselWrap.querySelector(".prev-button");
+    const nextBtn = carouselWrap.querySelector(".next-button");
+
+    const card = carouselWrap.querySelector(".card");
+
+    const movement = scrollContainer.offsetWidth * 75 / 100;
+    const duration = 1000;
+
+    let atEnd = false;
+    let atStart = true;
+
+    prevBtn.addEventListener("click", () => {
+        if (atStart) {
+            const clonedCarousel = carousel.cloneNode(true);
+            scrollContainer.insertBefore(clonedCarousel, scrollContainer.firstChild);
+            scrollContainer.style.gap = getComputedStyle(carousel).gap;
+
+            scrollContainer.scrollLeft = clonedCarousel.offsetWidth;
+
+            smoothScroll(scrollContainer, -movement, duration);
+
+            atStart = false;
+            atEnd = false;
+        } else {
+            smoothScroll(scrollContainer, -movement, duration);
+        }
+    });
+
+    nextBtn.addEventListener("click", () => {
+        if (atEnd) {
+            const clonedCarousel = carousel.cloneNode(true);
+            scrollContainer.appendChild(clonedCarousel);
+            scrollContainer.style.gap = getComputedStyle(carousel).gap;
+
+            smoothScroll(scrollContainer, movement, duration);
+
+            atEnd = false;
+            atStart = true;
+        } else {
+            smoothScroll(scrollContainer, movement, duration);
+        }
+    });
+
+    scrollContainer.addEventListener('scroll', () => {
+        if (scrollContainer.scrollLeft + scrollContainer.clientWidth >= scrollContainer.scrollWidth) {
+            atEnd = true;
+        } else {
+            atEnd = false;
+        }
+
+        if (scrollContainer.scrollLeft === 0) {
+            atStart = true;
+        } else {
+            atStart = false;
+        }
+    });
+
+    addScrollEvent(scrollContainer);
+});
+
+// Agregar evento scroll a featured carousel
+addScrollEvent(document.querySelector('.featured-carousel-container'));
---
+++ TP3/EntregaFinal/js/login.js
@@ -0,0 +1,75 @@
+
+document.querySelectorAll('.password-toggle').forEach(button => {
+    button.addEventListener('click', function () {
+  
+      const passwordInput = button.previousElementSibling; 
+      
+      
+      const showPasswordIcon = button.querySelector('.show-password');
+      const hidePasswordIcon = button.querySelector('.hide-password');
+      
+      
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        showPasswordIcon.style.display = 'none';
+        hidePasswordIcon.style.display = 'inline';
+      } else {
+        passwordInput.type = 'password';
+        showPasswordIcon.style.display = 'inline';
+        hidePasswordIcon.style.display = 'none';
+      }
+    });
+  });
+
+
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  validateInputs();
+
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+};
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+
+const validateInputs = () => {
+
+  const emailValue = email.value.trim(); 
+  const passwordValue = password.value.trim();
+
+  if (emailValue === '') {
+    setError(email, 'Email is required');
+    isValid = false;
+  } else {
+    setSuccess(email);
+  }
+
+  if (passwordValue === '') {
+    setError(password, 'Password is required');
+    isValid = false;
+  } else {
+    setSuccess(password);
+  }
+
+}
