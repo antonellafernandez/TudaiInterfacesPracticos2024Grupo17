4bcb9f3087dbb991775e0113520c53111f98b087
candela <gcandela894@gmail.com>
2024-11-04 00:02:30 -0300

    estilos todo estilos

--- TP3/EntregaFinal/js/4_inline.js
+++ TP3/EntregaFinal/js/4_inline.js
@@ -92,17 +92,14 @@ function dibujarMensajeGanador(jugador) {
 
     // Muestra la pantalla de ganador
     document.getElementById('pantallaGanador').style.display = 'flex';
-<<<<<<< HEAD
-=======
     // Mostrar la pantalla de ganador después de 1 segundo y añadir clase visible
     setTimeout(() => {
         const pantallaGanador = document.getElementById('pantallaGanador');
         pantallaGanador.style.display = 'flex';
         pantallaGanador.classList.add('visible'); // Agregar clase visible para la transición
     }, 1000); // Tiempo antes de mostrar
-
->>>>>>> 6ae51da68e190d3001de794ab3cae53cf0fc5441
 }
+
 // Función para mostrar el mensaje de empate
 function dibujarMensajeEmpate() {
     const mensajeEmpate = document.getElementById('mensajeEmpate');
--- TP3/EntregaFinal/js/Game.js
+++ TP3/EntregaFinal/js/Game.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("closeAside").addEventListener("click", function() {
-        const aside = document.querySelector(".card");
+    const closeAsideBtn = document.getElementById("closeAside");
+    const showAsideBtn = document.getElementById("showAside");
+    const aside = document.querySelector(".card");
+
+    // Al hacer clic en "Cerrar", ocultar el aside y mostrar el botón "How to Play"
+    closeAsideBtn.addEventListener("click", function() {
         aside.style.display = "none"; // Oculta el aside
+        showAsideBtn.style.display = "block"; // Muestra el botón "How to Play"
+    });
+
+    // Al hacer clic en "How to Play", mostrar el aside y ocultar el botón
+    showAsideBtn.addEventListener("click", function() {
+        aside.style.display = "block"; // Muestra el aside
+        showAsideBtn.style.display = "none"; // Oculta el botón "How to Play"
     });
 });
