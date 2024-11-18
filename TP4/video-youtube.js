e0d8e50a6f9d5d5bfed7b93f11f89492ded6b241
Daniela Fernández <liquiddookie.17@gmail.com>
2024-11-18 18:36:59 -0300

    Updated.

--- TP4/video-youtube.js
+++ TP4/video-youtube.js
@@ -1,17 +1,1 @@
-// Escuchar el evento de scroll
-window.addEventListener('scroll', function() {
-    // Obtener la posición actual del scroll
-    let scrollPosition = window.scrollY;
 
-    // Ajustar la velocidad del movimiento para cada elemento
-    let video = document.querySelector('.video');
-    let numero3 = document.querySelector('.numero-3');
-
-    // Definir las velocidades de desplazamiento
-    let videoSpeed = 0.5; // El video se moverá a la mitad de la velocidad del scroll
-    let numero3Speed = 0.2; // El personaje se moverá más despacio
-
-    // Aplicar el transform para mover el video y el personaje
-    video.style.transform = 'translateY(' + scrollPosition * videoSpeed + 'px)';
-    numero3.style.transform = 'translateY(' + scrollPosition * numero3Speed + 'px)';
-});
